b43b3588797a573bcb44a4bf5b6bc6b9d882acb7
Alex Middeleer <amiddeleer@gmail.com>
2014-03-13 17:45:12 -0400

    Logmon now uses a helper, and does some nice filtering.

---
+++ trackerHelper.js
@@ -0,0 +1,58 @@
+var tailMod = require( "file-tail" );
+var fs = require("fs");
+
+module.exports = {
+	"helpWatch" : function(fd) {
+		var letters;
+		
+		var logMemory = [];
+		var newStuff = [];
+	
+		var tailer = tailMod.startTailing(fd);
+	
+	  // -----------------------------------------------------
+	  // Remember last x additions to log file, remembering only
+		// new types of entries.  In addition, keep a separate 
+		// list of log entries that have not yet been retrieved 
+		// with the "getAnyNewEntries" method.
+	  // -----------------------------------------------------
+		tailer.on('line',function(line) {
+			letters = justLetters(line);
+			debugLog("got a new line... :" + line);
+	
+			if (logMemory.indexOf(letters) < 0) {
+				logMemory.push(letters);
+				newStuff.push(line);
+			}
+			
+			if(logMemory.length > 1000){
+				logMemory.shift();
+			}
+			if(newStuff.length > 1000){
+				newStuff.shift();
+			}
+	
+			debugLog("logMemory length is now: " + logMemory.length);
+			debugLog("newStuff length is now: " + newStuff.length);
+		});
+		
+		var helper = {};
+		
+		helper.getAnyNewEntries = function() {
+			var result = newStuff.slice(0);
+			newStuff = [];
+			debugLog("returning new stuff: " + result);
+			return result;
+		};
+	
+		return helper;
+	}
+}
+
+function justLetters (s) {
+	return s.replace(/[^A-Za-z]/g, "");
+}
+
+function debugLog (text) {
+	fs.appendFile('logmon-helper-debug.log', text + '\n');
+}
--- trackers/logmon.js
+++ trackers/logmon.js
@@ -9,7 +9,7 @@ var valise = require( "valise" )
 	, async = require( "async" )
 	, _ = require( "underscore" )
 	, statID = "logs"
-	, tailmod = require( "file-tail" )
+	, logMonHelper = require( "../trackerHelper" )
 	, bufferMax = 100
 	;
 
@@ -17,46 +17,15 @@ function debugLog (text) {
 	fs.appendFile('logmon-debug.log', text + '\n');
 }
 
-process.on('error', function(e) {debugLog(e);	});
-
-function addToBuffer (line, buffer) {
-	if(buffer.length > bufferMax){
-		buffer.shift();
-	}
-	buffer.push(line);
-}
-
-var fileObjs = logmonConf.logFiles;
-var fileTailObjs = [];
-var fileTailBuffers = [];
-
-_.each(fileObjs, function(logFileConf) {
-	var fd = logFileConf["location"]
-	,   tailObj = tailmod.startTailing(fd);
-	debugLog("Watching.. " + fd)
-	fileTailObjs.push(tailObj);
+// -----------------------------------------------------
+// Create a trackerHelper for each tracked logfile
+// -----------------------------------------------------
+var helpers = [];
+var fds = _.pluck(logmonConf.logFiles, 'location');
+_.each(fds, function(fd) {
+	debugLog("watching: " + fd);
+	helpers.push(logMonHelper.helpWatch(fd));
 });
-
-//function justLetters (s) {
-//	return s.replace(/[^A-Za-z]/g, "");
-//}
-
-for (var i = 0; i < fileTailObjs.length; i++) {
-
-	var tailer = fileTailObjs[i];
-
-	(function() {
-
-		var buffer = [];
-
-		tailer.on('line',function(line) {
-				buffer.push(line);
-		})
-		
-		fileTailBuffers.push(buffer);
-	})();
-}
-
 
 // -----------------------------------------------------
 // Export as a method that takes a "done" callback
@@ -70,20 +39,17 @@ module.exports = (function(){
 		// -----------------------------------------------------
 		// Retrieve any info from each of our log file buffers
 		// -----------------------------------------------------
-		var i
-		,   buffer;
-		
 		var loggerFunc = function(line, name) {
 				debugLog("Logging line: " + line);
 				logger.info(name + ": " + line);
 		};
 
-		for (i = 0; i < fileObjs.length; i++) {
-			buffer = fileTailBuffers[i];
-			while(buffer.length>0){
-				loggerFunc(buffer.pop(), fileObjs[i].name);
-			}
-		}
+		_.each(helpers,function(helper) {
+			var newStuff = helper.getAnyNewEntries()
+			_.each(newStuff, function(line){
+				loggerFunc(line, "logfilename");
+			});
+		});
 
 		cb();
 	};
